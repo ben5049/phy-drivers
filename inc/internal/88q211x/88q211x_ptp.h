7ec0803b2cb13c225ffd4d3d874e27ea34765534
ben-linux <bens15351@gmail.com>
2026-07-10 00:01:07 +0100

    [88q211x] Updated TX/RX latencies

--- inc/internal/88q211x/88q211x_ptp.h
+++ inc/internal/88q211x/88q211x_ptp.h
@@ -16,12 +16,15 @@ extern "C" {
 #include "88q211x.h"
 
 
-/* Latencies from https://intrepidcs.com/products/automotive-ethernet-tools/rad-gigastar-1000base-t1-active-tap-gateway-vehicle-interface/rad-gigastar-specifications/
- * Since no official Marvell or Infineon sources could be found */
-#define PHY_88Q211X_100M_INGRESS_LATENCY (780) /* ns. TODO: Check this is the same as 1G */
-#define PHY_88Q211X_1G_INGRESS_LATENCY   (780) /* ns */
-#define PHY_88Q211X_100M_EGRESS_LATENCY  (240) /* ns. TODO: Check this is the same as 1G */
-#define PHY_88Q211X_1G_EGRESS_LATENCY    (240) /* ns */
+/* No official Marvell or Infineon sources could be found so the following
+ * sources were used in addition to finding them experimentally:
+ * - DP83TC815-Q1:   100BASE-T1 https://www.ti.com/lit/gpn/DP83TC815-Q1
+ * - DP83TG720R-Q1: 1000BASE-T1 https://www.ti.com/lit/gpn/DP83TG720R-Q1
+ */
+#define PHY_88Q211X_100M_INGRESS_LATENCY (960)  /* ns (estimated) */
+#define PHY_88Q211X_1G_INGRESS_LATENCY   (4400) /* ns (estimated), larger than others due to FEC */
+#define PHY_88Q211X_100M_EGRESS_LATENCY  (480)  /* ns (estimated) */
+#define PHY_88Q211X_1G_EGRESS_LATENCY    (480)  /* ns (estimated) */
 
 
 #ifdef __cplusplus
